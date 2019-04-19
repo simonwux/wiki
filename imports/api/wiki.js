733aa6551284eec233fc050b9b1ce885b57ffba3
Simon <simonwux@126.com>
2019-04-19 10:20:21 -0700

    first commit

---
+++ imports/api/wiki.js
@@ -0,0 +1,35 @@
+
+import { Mongo } from "meteor/mongo";
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+var wikipedia = require("node-wikipedia");
+
+//export const Wiki = new Mongo.Collection("Wiki");
+
+if (Meteor.isServer) {
+  Meteor.publish("messages", function messagesPublish() {
+    return Messages
+      .find({}, {
+        limit: 10,
+        sort: {
+          createdAt: -1
+        }
+      });
+  });
+}
+
+
+Meteor.methods({
+  "messages.insert"(message)  {
+    check(message, String);
+
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    return new Promise((resolve, reject) => {
+          wikipedia.page.data(message, { content: true }, resolve);
+    });
+  }
+});
---
+++ imports/ui/wiki.jsx
@@ -0,0 +1,16 @@
+var wikipedia = require("node-wikipedia");
+
+wikipedia.page.data("Clifford_Brown", { content: true }, function(response) {
+	// structured information on the page for Clifford Brown (wikilinks, references, categories, etc.)
+});
+
+wikipedia.revisions.all("Miles_Davis", { comment: true }, function(response) {
+	// info on each revision made to Miles Davis' page
+});
+
+wikipedia.categories.tree(
+	"Philadelphia_Phillies",
+	function(tree) {
+		//nested data on the category page for all Phillies players
+	}
+);
